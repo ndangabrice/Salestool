550c55693a9a735e6114149bf02f2665d55f55a2
ndangabrice <ndangabrice@gmail.com>
2015-10-25 09:54:03 +0100

    initial check in of sales tool.

---
+++ SalesTool/src/com/example/salestool/Main.java
@@ -0,0 +1,14 @@
+package com.example.salestool;
+
+public class Main {
+	public static void main(String args){
+		SalesData data = new SalesData ();
+		
+		displayGreeting();
+		data.display();
+	} 
+	
+	private static void displayGreeting()
+	{System.out.println("HELLO HAPPY SALES PEOPLE !");
+	System.out.println("THIS APP SHOW SALES DATA");}
+}
